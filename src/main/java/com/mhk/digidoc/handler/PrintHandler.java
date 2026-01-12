db737ced7bacb60668464b14acd4ea17e6789fa6
hardiksoni <hardikbiomed@gmail.com>
2026-01-13 00:08:45 +0530

    Lambda changes

--- src/main/java/com/mhk/digidoc/handler/PrintHandler.java
+++ src/main/java/com/mhk/digidoc/handler/PrintHandler.java
@@ -1,23 +1,21 @@
 package com.mhk.digidoc.handler;
 
-//import com.amazonaws.services.lambda.runtime.Context;
-//import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.mhk.digidoc.entity.ReportWrapper;
 import com.mhk.digidoc.pdf.PDFReportGenerator;
 
 import java.io.ByteArrayOutputStream;
 import java.util.Base64;
 
-//public class PrintHandler implements RequestHandler<ReportWrapper, String> {
-    public class PrintHandler  {
+public class PrintHandler implements RequestHandler<ReportWrapper, String> {
 
-    //@Override
-    //public String handleRequest(ReportWrapper wrapper, Context context) {
-        public String handleRequest(ReportWrapper wrapper) {
+    @Override
+    public String handleRequest(ReportWrapper wrapper, Context context) {
         // Log input
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         PDFReportGenerator reportGenerator = new PDFReportGenerator();
-       // reportGenerator.loadHTML(byteArrayOutputStream,wrapper.getPatient(),wrapper.getPatientReport(),wrapper.getTenant(),new String(Base64.getDecoder().decode(wrapper.getReportTemplate())));
+        reportGenerator.loadHTML(byteArrayOutputStream, wrapper.getPatient(), wrapper.getPatientReport(), wrapper.getReportContent());
         byte[] pdfBytes = byteArrayOutputStream.toByteArray();
         return Base64.getEncoder().encodeToString(pdfBytes);
     }
